66ce6d93c814fe89e84cd1d81d133a2faa1912ec
Lucas Domingos Leão Gomes <lgomes@dannemann.com.br>
2019-04-18 08:02:23 -0300

    Usando o Material

--- material-app/src/app/app.module.ts
+++ material-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule // Importar para usar os botoes do Angular Material
   ],
   providers: [],
   bootstrap: [AppComponent]
